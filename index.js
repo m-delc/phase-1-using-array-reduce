ce6732758ca0f8b0f889d84dd0cd51cda8c89fdf
MichaelDelcambre <delcambrem@gmail.com>
2021-11-24 13:34:22 -0600

    done

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+// //accumulator = 0, element = 4 accumuloator now 4 (for the first pass through)
+// //accumulator = 4, element = 5, accumulator value now = 9
+// batteryBatches.reduce(function(accumulator, element){
+//     return accumulator + element
+//     }, 0)
+
+const totalBatteries = batteryBatches.reduce((accumulator,element) => accumulator + element);
